28a05a57fe917351f2b2b75f6164896811ce2837
vladamihaesei <vlad.amihaesei95@gmail.com>
2020-11-16 15:06:03 +0200

    Delete stack_raster_correlation_2.R

--- stack_raster_correlation_2.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-library(raster)
-library(RColorBrewer)
-library(rnaturalearth)
-
-###granite pentru layout nu este obligatoriu
-ne_countries()
-granite <- list("sp.polygons", ne_countries(scale =10),col = "black",lwd = 1.5,first=FALSE)
